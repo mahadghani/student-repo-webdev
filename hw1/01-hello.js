91c0df1e01cb4b2b5e7f4b73d733aaa9023a5718
mahadghani <mghani@pdx.edu>
2023-07-11 17:06:22 -0700

    homework1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Mahad Ghani and I will be taking CS 463 this term to gain an introduction to Web Dev. I am pursuing a Bachelor's Degree in Computer Science and entering my final year. I like wathcing MMA and hiking in my free time!"
+  );
 }
