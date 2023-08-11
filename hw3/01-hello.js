33c12e11a5c3a6426841bf16606c072b832ae115
mahadghani <mghani@pdx.edu>
2023-08-11 00:01:34 -0700

    Homework 3 08/11/2023

--- hw3/01-hello.js
+++ hw3/01-hello.js
@@ -1,1 +1,27 @@
-// Add your code here
+// Mahad S. Ghani | CS463 | HW3/01-hello.js
+
+// Create an img element
+var imgElement = document.createElement("img");
+imgElement.src = "../images/mahad.JPG"; // Replace with your image URL
+imgElement.alt = "Personal Photo";
+imgElement.width = 200; // Set the desired width
+imgElement.id = "selfImg";
+document.body.append(imgElement);
+
+// Create a paragraph element
+var paragraphElement = document.createElement("p");
+paragraphElement.textContent =
+  "My name is Mahad Ghani and I will be taking CS 463 this term to gain an introduction to Web Dev. I am pursuing a Bachelor's Degree in Computer Science and entering my final year. I like wathcing MMA and hiking in my free time!";
+paragraphElement.id = "bio";
+document.body.append(paragraphElement);
+
+// Make first sentence bold
+var firstSentence = paragraphElement.textContent.split(".")[0]; // split text into array separating using periods. use first
+var spanElement = document.createElement("span"); // make span tag
+spanElement.textContent = firstSentence;
+spanElement.style.fontWeight = "bold"; // make it bold
+paragraphElement.innerHTML = paragraphElement.innerHTML.replace(
+  // repalce old with new bolded sentence
+  firstSentence,
+  spanElement.outerHTML
+);
--- hw3/02-form.js
+++ hw3/02-form.js
@@ -1,1 +1,25 @@
 // Add your code here
+let pronouns;
+const radioButtons = document.querySelectorAll('input[name="pronouns"]');
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("click", (event) => {
+    pronouns = event.target.value;
+  });
+});
+
+let example = document.querySelector("#submit");
+let nameR = document.querySelector("#name");
+let username = document.querySelector("#username");
+let email = document.querySelector("#email");
+let dob = document.querySelector("#dob");
+
+example.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  console.log("Name: ", nameR.value);
+  console.log("Username: ", username.value);
+  console.log("Email: ", email.value);
+  console.log("Date of Birth: ", dob.value);
+  console.log("Pronouns: ", pronouns);
+}
